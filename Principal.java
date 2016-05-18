562392680118bdb9facfc8c61a8e424b53a17aa6
claudiogn <claudioguimaraes14@gmail.com>
2016-05-17 21:35:45 -0300

    Versão com GUI

--- Principal.java
+++ Principal.java
@@ -5,15 +5,17 @@
  */
 
 public class Principal {
-	public static void main(String[] args) {
-		ConvertEquation q = new ConvertEquation("A'BC + B + B'C'D");
+	
+                public void run(String eq){
+                    ConvertEquation q = new ConvertEquation(eq);
 		char[][] matriz = q.analise();
-		
 		for(int i = 0; i < matriz.length; i++ ){
 			for(int j = 0; j < matriz[0].length; j++){
 				System.out.print(matriz[i][j] + " ");
 			}
 			System.out.println("");
 		}
+            }
+    
+                
 	}
-}
